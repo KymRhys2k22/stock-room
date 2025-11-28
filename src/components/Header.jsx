1b7cf9d52ca4bf145bcf8521248e0831ad9f6aae
KymRhys2@k22 <kymrhys@gmail.com>
2025-11-28 22:55:17 +0800

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header({ title, headerText }) {
       <div className="w-8 h-8 bg-slate-900 rounded flex items-center justify-center text-white">
         <div className="w-4 h-0.5 bg-white mb-1"></div>
       </div>
-      <h1 className="text-xl font-bold text-slate-900">
+      <h1 className="text-xl text-center font-bold text-slate-900">
         {headerText} {text}
       </h1>
       <Filter className="w-6 h-6 text-slate-900" />
