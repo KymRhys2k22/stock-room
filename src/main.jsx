0a7372ab9269ebbbf77736410311375f4101cd05
KymRhys2@k22 <kymrhys@gmail.com>
2025-12-02 02:04:45 +0800

    productions

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,10 @@ import NotFound from "./components/NotFound.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-console.log(PUBLISHABLE_KEY);
 
-if (!PUBLISHABLE_KEY) {
+/* if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
-}
+} */
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
